e5776374d8af684e98c705296b1b83f46bae1366
tisk0405 <41276407+tisk0405@users.noreply.github.com>
2018-10-15 14:22:42 +0900

    메인화면 완성

---
+++ PCBaamSystem/PCBaamSystem/MainForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace PCBaamSystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.FindIdPw = new System.Windows.Forms.Button();
+            this.SignUpButton = new System.Windows.Forms.Button();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.IDLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::PCBaamSystem.Properties.Resources.mainscreen;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.ExitButton);
+            this.panel1.Controls.Add(this.LoginButton);
+            this.panel1.Controls.Add(this.IDLabel);
+            this.panel1.Controls.Add(this.FindIdPw);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.SignUpButton);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("함초롬바탕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // FindIdPw
+            // 
+            this.FindIdPw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.FindIdPw.BackColor = System.Drawing.Color.Black;
+            this.FindIdPw.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FindIdPw.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.FindIdPw.Location = new System.Drawing.Point(485, 379);
+            this.FindIdPw.Name = "FindIdPw";
+            this.FindIdPw.Size = new System.Drawing.Size(140, 39);
+            this.FindIdPw.TabIndex = 7;
+            this.FindIdPw.Text = "ID/PW 찾기";
+            this.FindIdPw.UseVisualStyleBackColor = false;
+            // 
+            // SignUpButton
+            // 
+            this.SignUpButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SignUpButton.BackColor = System.Drawing.Color.Black;
+            this.SignUpButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.SignUpButton.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.SignUpButton.Location = new System.Drawing.Point(333, 379);
+            this.SignUpButton.Name = "SignUpButton";
+            this.SignUpButton.Size = new System.Drawing.Size(146, 39);
+            this.SignUpButton.TabIndex = 6;
+            this.SignUpButton.Text = "회원가입";
+            this.SignUpButton.UseVisualStyleBackColor = false;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoginButton.BackColor = System.Drawing.Color.Black;
+            this.LoginButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.LoginButton.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.LoginButton.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.LoginButton.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.LoginButton.Location = new System.Drawing.Point(653, 271);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(118, 96);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "로그인";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            // 
+            // IDLabel
+            // 
+            this.IDLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.IDLabel.AutoSize = true;
+            this.IDLabel.BackColor = System.Drawing.Color.Transparent;
+            this.IDLabel.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.IDLabel.Font = new System.Drawing.Font("함초롬바탕", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.IDLabel.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.IDLabel.Location = new System.Drawing.Point(346, 279);
+            this.IDLabel.Name = "IDLabel";
+            this.IDLabel.Size = new System.Drawing.Size(138, 35);
+            this.IDLabel.TabIndex = 0;
+            this.IDLabel.Text = "아이디(ID)";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.label1.Font = new System.Drawing.Font("함초롬바탕", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.label1.Location = new System.Drawing.Point(307, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "비밀번호(PW)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(490, 323);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 33);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(490, 284);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.BackColor = System.Drawing.Color.Black;
+            this.ExitButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ExitButton.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.ExitButton.Location = new System.Drawing.Point(631, 379);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(140, 39);
+            this.ExitButton.TabIndex = 8;
+            this.ExitButton.Text = "종료";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label IDLabel;
+        private System.Windows.Forms.Button FindIdPw;
+        private System.Windows.Forms.Button SignUpButton;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
--- PCBaamSystem/PCBaamSystem/MainForm.cs
+++ PCBaamSystem/PCBaamSystem/MainForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace PCBaamSystem
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
